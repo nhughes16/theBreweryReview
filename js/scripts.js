6e464d446021053b6f53c78ebfff851f48d885ff
nhughes16 <34460434+nhughes16@users.noreply.github.com>
2021-02-06 10:17:01 -0500

    build update

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('#btnReview1').click(function () {
+        $('#review1Modal').modal('show');
+    });
+    $('#btnReview2').click(function () {
+        $('#review2Modal').modal('show');
+    });
+    $('#btnReview3').click(function () {
+        $('#review3Modal').modal('show');
+    });
+});
